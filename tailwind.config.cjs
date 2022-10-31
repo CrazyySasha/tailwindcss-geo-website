59c087ea5ef0d242eb172e9a15476a299a9a485c
Akromovich <akromovich0928@gmail.com>
2022-11-01 02:21:06 +0500

    edited many things in header

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -17,7 +17,7 @@ module.exports = {
     supports: {},
     colors: ({ colors }) => ({
       primary: "#009A67",
-
+      redi:'#FF0000',
       secondary: "#333333",
       borders: "#D6DBE1",
 
@@ -229,7 +229,7 @@ module.exports = {
       DEFAULT: '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)',
       md: '0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)',
       lg: '0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)',
-      xl: '0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1)',
+      xl: '0 18px 40px -10px #009A67, 0 8px 10px -6px rgb(0 0 0 / 0.1)',
       '2xl': '0 25px 50px -12px rgb(0 0 0 / 0.25)',
       inner: 'inset 0 2px 4px 0 rgb(0 0 0 / 0.05)',
       none: 'none',
@@ -302,6 +302,7 @@ module.exports = {
       xl: ['0 20px 13px rgb(0 0 0 / 0.03)', '0 8px 5px rgb(0 0 0 / 0.08)'],
       '2xl': '0 25px 25px rgb(0 0 0 / 0.15)',
       none: '0 0 #0000',
+      button:'0 10px 10px #009A67',
     },
     fill: ({ theme }) => ({
       none: 'none',
